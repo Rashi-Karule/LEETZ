0892f19720a781452001d3e0a46ef5addadd5310
Rashi Karule <karulerashi@gmail.com>
2026-04-11 01:51:33 +0530

    Time: 0 ms (100%), Space: 13.6 MB (57.36%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -2,11 +2,15 @@ class Solution(object):
     def moveZeroes(self, nums):
         """
         :type nums: List[int]
-        :rtype: None
+        :rtype: None Do not return anything, modify nums in-place instead.
         """
-        j = 0  # position for next non-zero
-
+        j = 0 #i pura array traverse karega
+        # j → batata hai next non-zero kahan rakhna hai
+        #Jab non-zero mile:
+        #usko aage shift karo (swap)
+        #j++
         for i in range(len(nums)):
             if nums[i] != 0:
                 nums[j], nums[i] = nums[i], nums[j]
                 j += 1
+        
