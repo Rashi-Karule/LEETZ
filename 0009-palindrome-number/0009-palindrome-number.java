7b45d7043a773ea7f0b21cd3d4c02ba5c8cd6a78
Rashi Karule <karulerashi@gmail.com>
2026-05-10 17:35:05 +0530

    Time: 5 ms (79.33%), Space: 46.1 MB (17.55%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int rev = 0;
+        int temp = x;
+
+        while(temp > 0){
+            int digit = temp % 10;
+            rev = rev * 10 + digit;
+            temp = temp / 10;
+        }
+        if(x == rev){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+}
